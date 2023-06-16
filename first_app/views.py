643112d5f4e06ed689a446568e88b09f18e90b5a
satuhin95 <saifcm95@gmail.com>
2023-06-16 21:02:04 +0600

    url , view

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("<h1>This is home page</h1> <br><a href='contact/'> Contact</a> <br><a href='about/'> About</a>")
+
+def contact(request):
+    return HttpResponse("<h1>This is contact page</h1> <br><a href='/'> Home</a> <br><a href='/about/'> About</a>")
+
+def about(request):
+    return HttpResponse("<h1>This is about page</h1> <br><a href='/contact/'> Contact</a> <br><a href='/'> Home</a>")
