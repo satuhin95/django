0ef9cea48823d6c5488b776067957f038b4dabbb
satuhin95 <saifcm95@gmail.com>
2023-06-17 09:38:10 +0600

    Show data in views

--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Musician, Album
 # Create your views here.
 
 
 def home(request):
-    diction ={'name':"Comilla Victoria Govt College"}
-
+    musicions = Musician.objects.order_by('first_name')
+    diction ={'name':"Comilla Victoria Govt College",'musicions':musicions}
     return render(request, 'first_app/index.html', context=diction)
