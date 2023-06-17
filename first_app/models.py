b42eb69b6f0f1bf8e8ffa9c940ee384d64674f54
satuhin95 <saifcm95@gmail.com>
2023-06-17 09:02:14 +0600

    model

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+    
+class Musician(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    artist  = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()   
+    num_stars = models.IntegerField()   
+
+    def __str__(self) :
+        return self.first_name +"  "+ self.last_name
