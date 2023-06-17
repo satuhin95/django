f551a06f25b6181a2ed5f5f175c1a2da48dfaf73
satuhin95 <saifcm95@gmail.com>
2023-06-17 09:25:59 +0600

    model update

--- first_app/models.py
+++ first_app/models.py
@@ -10,12 +10,25 @@ class Musician(models.Model):
     last_name = models.CharField(max_length=30)
     instrument = models.CharField(max_length=100)
 
+    def __str__(self) :
+        return self.first_name +"  "+ self.last_name
+
 class Album(models.Model):
     id = models.AutoField(primary_key=True)
     artist  = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()   
-    num_stars = models.IntegerField()   
+
+    rating =(
+        (1, 'Worst'),
+        (2, 'Bad'),
+        (3, 'Not Bad'),
+        (4, 'Good'),
+        (5, 'Excellent!'),
+    )
+    num_stars = models.IntegerField(choices=rating) 
 
     def __str__(self) :
-        return self.first_name +"  "+ self.last_name
+        return self.name +", Rating:  "+ str(self.num_stars)  
+
+    
