301d2b964c9b28eb6999fbe76a1b8c34870c61df
satuhin95 <saifcm95@gmail.com>
2023-06-18 12:17:34 +0600

    form submission

--- first_app/forms.py
+++ first_app/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 class user_form(forms.Form):
     user_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':"Enter Your Name",'style':"width:150px"}))
-    user_email = forms.EmailField()
+    user_email = forms.EmailField(label='User Email',widget=forms.TextInput(attrs={'placeholder':"Enter Your Email"}))
     user_dob = forms.DateField(label='Date of Birth', widget=forms.TextInput(attrs={'type':'date'}))
+    
--- first_app/views.py
+++ first_app/views.py
@@ -14,4 +14,17 @@ def home(request):
 def form(request):
    new_form = forms.user_form()
    diction = {'test_form':new_form,'heading_1':'Django Form Library'}
+   if request.method  == 'POST':
+       new_form = forms.user_form(request.POST)
+
+       if new_form.is_valid():
+           user_name = new_form.cleaned_data['user_name']
+           user_email = new_form.cleaned_data['user_email']
+           user_dob = new_form.cleaned_data['user_dob']
+
+           diction.update({'user_name':user_name})
+           diction.update({'user_email':user_email})
+           diction.update({'user_dob':user_dob})
+           diction.update({'form_submited':'yes'})
+
    return render(request, 'first_app/form.html', context=diction ) 
