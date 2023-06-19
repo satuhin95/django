075b0a1dcedfde8bfcd857139266efbabad93787
satuhin95 <saifcm95@gmail.com>
2023-06-19 22:49:36 +0600

    edit and update complate with mysql

--- first_app/urls.py
+++ first_app/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('album/', views.album_list, name='album'),
     path('add-album/', views.add_album, name='add_album'),
     path('add-musician/', views.musician_form, name='musician_form'),
+    path('musician-details/<int:id>/', views.musician_details, name='musician_details'),
+    path('musician-edit/<int:id>/', views.musician_edit, name='musician_edit'),
+    path('musician-delete/<int:id>/', views.musician_delete, name='musician_delete'),
+    path('album-edit/<int:id>/', views.album_edit, name='album_edit'),
+    path('album-delete/<int:id>/', views.album_delete, name='album_delete'),
 
   
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Musician, Album
 from first_app import forms
+from django.db.models import Avg, Max
 # Create your views here.
 
 
@@ -14,6 +15,26 @@ def album_list(request):
     albums = Album.objects.order_by('id')
     diction = {'title':'List of Album','albums':albums}
     return render(request,'first_app/album_list.html', context=diction)
+def musician_details(request,id):
+    musicion = Musician.objects.get(pk=id)
+    albums = Album.objects.filter(artist=id)
+    artist_reating = albums.aggregate(avg=Avg('num_stars'))
+    diction = {'title':'Musician Details','albums':albums,'musicion':musicion,'artist_reating':artist_reating}
+    return render(request,'first_app/musician_details.html', context=diction)
+
+def musician_edit(request,id):
+    musicion = Musician.objects.get(pk=id)
+    form = forms.MusicianForm(instance=musicion)
+    if request.method == "POST":
+        form = forms.MusicianForm(request.POST, instance=musicion)
+        
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+            
+
+    diction = {'title':'Musician Edit','edit_form':form}
+    return render(request,'first_app/musician_edit.html', context=diction)
 
 def musician_form(request):
     form = forms.MusicianForm()
@@ -39,4 +60,25 @@ def add_album(request):
 
     diction = {'title':'Add Album','album_form':form}
     return render(request,'first_app/add_album.html', context=diction)
+def album_edit(request,id):
+    album = Album.objects.get(pk=id)
+    form = forms.AlbumForm(instance=album)
+    diction ={}
+    if request.method == "POST":
+        form = forms.AlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save(commit=True)
+            diction.update({'message':"Update Successfully"})
+
+
+    diction.update({'title':'Edit Album','album_form':form})
+    return render(request,'first_app/album_edit.html', context=diction)
+def album_delete(request,id):
+    album = Album.objects.get(pk=id)
+    album.delete()
+    return album_list(request)
+def musician_delete(request,id):
+    musician = Musician.objects.get(pk=id)
+    musician.delete()
+    return index(request)
 
