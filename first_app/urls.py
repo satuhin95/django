4925fcf9a0578ca7efebbf298c39f8f8e197e683
satuhin95 <saifcm95@gmail.com>
2023-06-20 17:40:33 +0600

    template view

--- first_app/urls.py
+++ first_app/urls.py
@@ -6,15 +6,6 @@ from first_app import views
 app_name = "first_app"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('album/', views.album_list, name='album'),
-    path('add-album/', views.add_album, name='add_album'),
-    path('add-musician/', views.musician_form, name='musician_form'),
-    path('musician-details/<int:id>/', views.musician_details, name='musician_details'),
-    path('musician-edit/<int:id>/', views.musician_edit, name='musician_edit'),
-    path('musician-delete/<int:id>/', views.musician_delete, name='musician_delete'),
-    path('album-edit/<int:id>/', views.album_edit, name='album_edit'),
-    path('album-delete/<int:id>/', views.album_delete, name='album_delete'),
-
+    path('', views.IndexView.as_view() , name="index")
   
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,84 +1,15 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
-from first_app.models import Musician, Album
-from first_app import forms
-from django.db.models import Avg, Max
-# Create your views here.
+from django.views.generic import View, TemplateView
 
 
-def index(request):
-    musicions = Musician.objects.order_by('first_name')
-    diction ={'title':"Home Page",'musicions':musicions}
-    return render(request, 'first_app/index.html', context=diction)
 
-def album_list(request):
-    albums = Album.objects.order_by('id')
-    diction = {'title':'List of Album','albums':albums}
-    return render(request,'first_app/album_list.html', context=diction)
-def musician_details(request,id):
-    musicion = Musician.objects.get(pk=id)
-    albums = Album.objects.filter(artist=id)
-    artist_reating = albums.aggregate(avg=Avg('num_stars'))
-    diction = {'title':'Musician Details','albums':albums,'musicion':musicion,'artist_reating':artist_reating}
-    return render(request,'first_app/musician_details.html', context=diction)
+class IndexView(TemplateView):
+    template_name = 'first_app/index.html'
 
-def musician_edit(request,id):
-    musicion = Musician.objects.get(pk=id)
-    form = forms.MusicianForm(instance=musicion)
-    if request.method == "POST":
-        form = forms.MusicianForm(request.POST, instance=musicion)
-        
-        if form.is_valid():
-            form.save(commit=True)
-            return index(request)
-            
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Comilla Victoria Govt. College'
 
-    diction = {'title':'Musician Edit','edit_form':form}
-    return render(request,'first_app/musician_edit.html', context=diction)
-
-def musician_form(request):
-    form = forms.MusicianForm()
-
-    if request.method == "POST":
-        form = forms.MusicianForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-            return index(request)    
-
-    diction = {'title':'Add Musician','musician_form':form}
-    return render(request,'first_app/add_musician.html', context=diction)
-
-def add_album(request):
-    form = forms.AlbumForm()
-
-    if request.method == "POST":
-        form = forms.AlbumForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-            return album_list(request)
-
-
-    diction = {'title':'Add Album','album_form':form}
-    return render(request,'first_app/add_album.html', context=diction)
-def album_edit(request,id):
-    album = Album.objects.get(pk=id)
-    form = forms.AlbumForm(instance=album)
-    diction ={}
-    if request.method == "POST":
-        form = forms.AlbumForm(request.POST, instance=album)
-        if form.is_valid():
-            form.save(commit=True)
-            diction.update({'message':"Update Successfully"})
-
-
-    diction.update({'title':'Edit Album','album_form':form})
-    return render(request,'first_app/album_edit.html', context=diction)
-def album_delete(request,id):
-    album = Album.objects.get(pk=id)
-    album.delete()
-    return album_list(request)
-def musician_delete(request,id):
-    musician = Musician.objects.get(pk=id)
-    musician.delete()
-    return index(request)
-
+        return context
